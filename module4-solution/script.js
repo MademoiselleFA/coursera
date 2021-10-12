d38575aadd9bfb61dc3ca0b4ea1c95e5285b7f81
Florence Ann Caspe <91104608+MademoiselleFA@users.noreply.github.com>
2021-10-12 20:09:45 +0800

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
-names[2]="Jeremiah";
-names[3]="Angela";
-names[4]="Mariz";
-names[5]="Rouvan";
-names[6]="Veronica";
-names[7]="Jessa";
-names[8]="Sydney";
-names[9]="Clarence";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
